a9ca4b5a13b392f7371d98f177b730cbd0828ab9
Seiori <danegibson2001@hotmail.co.uk>
2023-01-23 13:41:35 +0000

    Add project files.

---
+++ Project Nesja/Form1.cs
@@ -0,0 +1,15 @@
+namespace Project_Nesja
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
