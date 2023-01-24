8b64234cdb596d3285c9bbaacee869646983135c
Seiori <danegibson2001@hotmail.co.uk>
2023-01-24 15:48:33 +0000

    Added UI Layout of App Added Visable Selection Display of Buttons Function Navigation System with Child Forms Title Panel Allows for Easy Moving of Application Colour Indication to show Selected Form

--- Project Nesja/Form1.Designer.cs
+++ Project Nesja/Form1.Designer.cs
@@ -30,38 +30,268 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
             this.Logo = new System.Windows.Forms.PictureBox();
+            this.PanelMenu = new System.Windows.Forms.Panel();
+            this.MiscButton = new FontAwesome.Sharp.IconButton();
+            this.GameButton = new FontAwesome.Sharp.IconButton();
+            this.AramButton = new FontAwesome.Sharp.IconButton();
+            this.ProfileButton = new FontAwesome.Sharp.IconButton();
+            this.RankedButton = new FontAwesome.Sharp.IconButton();
+            this.HomeButton = new FontAwesome.Sharp.IconButton();
+            this.PanelTitleBar = new System.Windows.Forms.Panel();
+            this.LabelFormTitle = new System.Windows.Forms.Label();
+            this.IconCurrentForm = new FontAwesome.Sharp.IconPictureBox();
+            this.PanelDesktop = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            this.PanelMenu.SuspendLayout();
+            this.PanelTitleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconCurrentForm)).BeginInit();
             this.SuspendLayout();
             // 
             // Logo
             // 
             this.Logo.Image = ((System.Drawing.Image)(resources.GetObject("Logo.Image")));
             this.Logo.InitialImage = null;
-            this.Logo.Location = new System.Drawing.Point(12, -23);
+            this.Logo.Location = new System.Drawing.Point(1, 12);
             this.Logo.Name = "Logo";
-            this.Logo.Size = new System.Drawing.Size(182, 177);
-            this.Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Logo.Size = new System.Drawing.Size(185, 72);
+            this.Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Logo.TabIndex = 0;
             this.Logo.TabStop = false;
             this.Logo.Click += new System.EventHandler(this.Logo_Click);
+            // 
+            // PanelMenu
+            // 
+            this.PanelMenu.AutoSize = true;
+            this.PanelMenu.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.PanelMenu.BackColor = System.Drawing.SystemColors.ControlText;
+            this.PanelMenu.Controls.Add(this.MiscButton);
+            this.PanelMenu.Controls.Add(this.GameButton);
+            this.PanelMenu.Controls.Add(this.AramButton);
+            this.PanelMenu.Controls.Add(this.ProfileButton);
+            this.PanelMenu.Controls.Add(this.RankedButton);
+            this.PanelMenu.Controls.Add(this.HomeButton);
+            this.PanelMenu.Controls.Add(this.Logo);
+            this.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelMenu.ForeColor = System.Drawing.SystemColors.Control;
+            this.PanelMenu.Location = new System.Drawing.Point(0, 0);
+            this.PanelMenu.Name = "PanelMenu";
+            this.PanelMenu.Size = new System.Drawing.Size(189, 586);
+            this.PanelMenu.TabIndex = 2;
+            this.PanelMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // MiscButton
+            // 
+            this.MiscButton.BackColor = System.Drawing.Color.Black;
+            this.MiscButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.MiscButton.FlatAppearance.BorderSize = 0;
+            this.MiscButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MiscButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MiscButton.IconChar = FontAwesome.Sharp.IconChar.Infinity;
+            this.MiscButton.IconColor = System.Drawing.Color.White;
+            this.MiscButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.MiscButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.MiscButton.Location = new System.Drawing.Point(0, 440);
+            this.MiscButton.Name = "MiscButton";
+            this.MiscButton.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.MiscButton.Size = new System.Drawing.Size(186, 56);
+            this.MiscButton.TabIndex = 6;
+            this.MiscButton.Text = "Misc";
+            this.MiscButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.MiscButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.MiscButton.UseVisualStyleBackColor = false;
+            this.MiscButton.Click += new System.EventHandler(this.MiscButton_Click);
+            // 
+            // GameButton
+            // 
+            this.GameButton.BackColor = System.Drawing.Color.Black;
+            this.GameButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.GameButton.FlatAppearance.BorderSize = 0;
+            this.GameButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GameButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.GameButton.IconChar = FontAwesome.Sharp.IconChar.ChartSimple;
+            this.GameButton.IconColor = System.Drawing.Color.White;
+            this.GameButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.GameButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.GameButton.Location = new System.Drawing.Point(0, 378);
+            this.GameButton.Name = "GameButton";
+            this.GameButton.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.GameButton.Size = new System.Drawing.Size(186, 56);
+            this.GameButton.TabIndex = 5;
+            this.GameButton.Text = "Game";
+            this.GameButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.GameButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.GameButton.UseVisualStyleBackColor = false;
+            this.GameButton.Click += new System.EventHandler(this.GameButton_Click);
+            // 
+            // AramButton
+            // 
+            this.AramButton.BackColor = System.Drawing.Color.Black;
+            this.AramButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.AramButton.FlatAppearance.BorderSize = 0;
+            this.AramButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AramButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.AramButton.IconChar = FontAwesome.Sharp.IconChar.ChartSimple;
+            this.AramButton.IconColor = System.Drawing.Color.White;
+            this.AramButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.AramButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AramButton.Location = new System.Drawing.Point(0, 316);
+            this.AramButton.Name = "AramButton";
+            this.AramButton.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.AramButton.Size = new System.Drawing.Size(186, 56);
+            this.AramButton.TabIndex = 4;
+            this.AramButton.Text = "Aram";
+            this.AramButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AramButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.AramButton.UseVisualStyleBackColor = false;
+            this.AramButton.Click += new System.EventHandler(this.AramButton_Click);
+            // 
+            // ProfileButton
+            // 
+            this.ProfileButton.BackColor = System.Drawing.Color.Black;
+            this.ProfileButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ProfileButton.FlatAppearance.BorderSize = 0;
+            this.ProfileButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProfileButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ProfileButton.IconChar = FontAwesome.Sharp.IconChar.HomeUser;
+            this.ProfileButton.IconColor = System.Drawing.Color.White;
+            this.ProfileButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.ProfileButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ProfileButton.Location = new System.Drawing.Point(0, 192);
+            this.ProfileButton.Name = "ProfileButton";
+            this.ProfileButton.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.ProfileButton.Size = new System.Drawing.Size(186, 56);
+            this.ProfileButton.TabIndex = 3;
+            this.ProfileButton.Text = "Profile";
+            this.ProfileButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ProfileButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.ProfileButton.UseVisualStyleBackColor = false;
+            this.ProfileButton.Click += new System.EventHandler(this.ProfileButton_Click);
+            // 
+            // RankedButton
+            // 
+            this.RankedButton.BackColor = System.Drawing.Color.Black;
+            this.RankedButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.RankedButton.FlatAppearance.BorderSize = 0;
+            this.RankedButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RankedButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.RankedButton.IconChar = FontAwesome.Sharp.IconChar.ChartSimple;
+            this.RankedButton.IconColor = System.Drawing.Color.White;
+            this.RankedButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.RankedButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.RankedButton.Location = new System.Drawing.Point(0, 254);
+            this.RankedButton.Name = "RankedButton";
+            this.RankedButton.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.RankedButton.Size = new System.Drawing.Size(186, 56);
+            this.RankedButton.TabIndex = 2;
+            this.RankedButton.Text = "Ranked";
+            this.RankedButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.RankedButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.RankedButton.UseVisualStyleBackColor = false;
+            this.RankedButton.Click += new System.EventHandler(this.RankedButton_Click);
+            // 
+            // HomeButton
+            // 
+            this.HomeButton.BackColor = System.Drawing.Color.Black;
+            this.HomeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.HomeButton.FlatAppearance.BorderSize = 0;
+            this.HomeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.HomeButton.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.HomeButton.IconColor = System.Drawing.Color.White;
+            this.HomeButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.HomeButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.HomeButton.Location = new System.Drawing.Point(0, 130);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.HomeButton.Size = new System.Drawing.Size(186, 56);
+            this.HomeButton.TabIndex = 1;
+            this.HomeButton.Text = "Home";
+            this.HomeButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.HomeButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.HomeButton.UseVisualStyleBackColor = false;
+            this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
+            // 
+            // PanelTitleBar
+            // 
+            this.PanelTitleBar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PanelTitleBar.Controls.Add(this.LabelFormTitle);
+            this.PanelTitleBar.Controls.Add(this.IconCurrentForm);
+            this.PanelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelTitleBar.Location = new System.Drawing.Point(189, 0);
+            this.PanelTitleBar.Name = "PanelTitleBar";
+            this.PanelTitleBar.Size = new System.Drawing.Size(870, 75);
+            this.PanelTitleBar.TabIndex = 3;
+            this.PanelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PanelTitleBar_MouseDown);
+            // 
+            // LabelFormTitle
+            // 
+            this.LabelFormTitle.AutoSize = true;
+            this.LabelFormTitle.Location = new System.Drawing.Point(58, 18);
+            this.LabelFormTitle.Name = "LabelFormTitle";
+            this.LabelFormTitle.Size = new System.Drawing.Size(40, 15);
+            this.LabelFormTitle.TabIndex = 1;
+            this.LabelFormTitle.Text = "Home";
+            this.LabelFormTitle.Click += new System.EventHandler(this.LabelFormTitle_Click);
+            // 
+            // IconCurrentForm
+            // 
+            this.IconCurrentForm.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.IconCurrentForm.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.IconCurrentForm.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.IconCurrentForm.IconColor = System.Drawing.SystemColors.ControlLightLight;
+            this.IconCurrentForm.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.IconCurrentForm.Location = new System.Drawing.Point(20, 9);
+            this.IconCurrentForm.Name = "IconCurrentForm";
+            this.IconCurrentForm.Size = new System.Drawing.Size(32, 32);
+            this.IconCurrentForm.TabIndex = 0;
+            this.IconCurrentForm.TabStop = false;
+            this.IconCurrentForm.Click += new System.EventHandler(this.IconCurrentForm_Click);
+            // 
+            // PanelDesktop
+            // 
+            this.PanelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelDesktop.Location = new System.Drawing.Point(189, 75);
+            this.PanelDesktop.Name = "PanelDesktop";
+            this.PanelDesktop.Size = new System.Drawing.Size(870, 511);
+            this.PanelDesktop.TabIndex = 4;
+            this.PanelDesktop.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelDesktop_Paint);
             // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(1059, 586);
-            this.Controls.Add(this.Logo);
+            this.Controls.Add(this.PanelDesktop);
+            this.Controls.Add(this.PanelTitleBar);
+            this.Controls.Add(this.PanelMenu);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.Name = "MainMenu";
             this.Text = "Project Nesja";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            this.PanelMenu.ResumeLayout(false);
+            this.PanelTitleBar.ResumeLayout(false);
+            this.PanelTitleBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconCurrentForm)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private PictureBox Logo;
+        private Panel PanelMenu;
+        private FontAwesome.Sharp.IconButton HomeButton;
+        private FontAwesome.Sharp.IconButton MiscButton;
+        private FontAwesome.Sharp.IconButton GameButton;
+        private FontAwesome.Sharp.IconButton AramButton;
+        private FontAwesome.Sharp.IconButton ProfileButton;
+        private FontAwesome.Sharp.IconButton RankedButton;
+        private Panel PanelTitleBar;
+        private Label LabelFormTitle;
+        private FontAwesome.Sharp.IconPictureBox IconCurrentForm;
+        private Panel PanelDesktop;
     }
 }
--- Project Nesja/Form1.cs
+++ Project Nesja/Form1.cs
@@ -1,14 +1,21 @@
 using System.Diagnostics;
-using System.Security.Policy;
-using System.Text.Json.Nodes;
+using System.Runtime.InteropServices;
+using FontAwesome.Sharp;
 
 namespace Project_Nesja
 {
     public partial class MainMenu : Form
     {
+        private IconButton currentButton;
+        private Panel leftBorderButton;
+        private Form currentChildForm;
+        
         public MainMenu()
         {
             InitializeComponent();
+            leftBorderButton = new Panel();
+            leftBorderButton.Size = new Size(7, 60);
+            PanelMenu.Controls.Add(leftBorderButton);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -18,6 +25,7 @@ namespace Project_Nesja
 
         private void Logo_Click(object sender, EventArgs e)
         {
+            // Opens GitHub Associated with Project
             var psi = new ProcessStartInfo
             {
                 FileName = "https://github.com/Seiori/Project-Nesja",
@@ -25,5 +33,137 @@ namespace Project_Nesja
             };
             Process.Start(psi);
         }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender, Color.White);
+            OpenChildForm(new Form());
+        }
+
+        private void ProfileButton_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender, Color.White);
+            OpenChildForm(new Form());
+        }
+
+        private void RankedButton_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender, Color.White);
+            OpenChildForm(new Form());
+        }
+
+        private void AramButton_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender, Color.White);
+            OpenChildForm(new Form());
+        }
+
+        private void GameButton_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender, Color.White);
+            OpenChildForm(new Form());
+        }
+
+        private void MiscButton_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender, Color.White);
+            OpenChildForm(new Form());
+        }
+
+        private void IconCurrentForm_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LabelFormTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenChildForm(Form childForm)
+        {
+            if (currentChildForm != null)
+            {
+                currentChildForm.Close();
+            }
+            currentChildForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            PanelDesktop.Controls.Add(childForm);
+            PanelDesktop.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            LabelFormTitle.Text = childForm.Text;
+        }
+
+        private void ActiveButton(object senderButton, Color customColor)
+        {
+            if (senderButton != null)
+            {
+                // Disables Previous Button
+                DisableButton();
+
+                // Applies Effects to Currently Selected Button on the Side Bar
+                currentButton = (IconButton)senderButton;
+                currentButton.BackColor = Color.Gray;
+                currentButton.ForeColor = customColor;
+                currentButton.IconColor = customColor;
+                currentButton.TextAlign = ContentAlignment.MiddleCenter;
+                currentButton.ImageAlign = ContentAlignment.MiddleRight;
+                currentButton.TextImageRelation = TextImageRelation.TextBeforeImage;
+
+                // Applies Different Look to the Left Border of the Side Bar to show Selected Button
+                leftBorderButton.BackColor = customColor;
+                leftBorderButton.Location = new Point(0, currentButton.Location.Y);
+                leftBorderButton.Visible = true;
+                leftBorderButton.BringToFront();
+
+                // Sets the Icon of the Current Form
+                IconCurrentForm.IconChar = currentButton.IconChar;
+                IconCurrentForm.IconColor = customColor;
+
+                // Sets the Text of the Current Form
+                LabelFormTitle.Text = currentButton.Text;
+            }
+        }
+
+        private void DisableButton()
+        {
+            if (currentButton != null)
+            {
+                // Returns the Previously Selected Button to its Original State
+                currentButton.BackColor = Color.Black;
+                currentButton.ForeColor = Color.Gainsboro;
+                currentButton.IconColor = Color.Gainsboro;
+                currentButton.TextAlign = ContentAlignment.MiddleLeft;
+                currentButton.ImageAlign = ContentAlignment.MiddleLeft;
+                currentButton.TextImageRelation = TextImageRelation.ImageBeforeText;
+            }
+        }
+
+        private void PanelDesktop_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PanelTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, 0xA1, 0x2, 0);
+            }
+        }
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
     }
 }
